e3b4902673a7b744beaec46a7589980ceffa4b34
Cherry-2023 <Cherry5466@gmail.com>
2023-10-09 10:39:47 -0300

    subiendo frontend projeto Control Pacientes Veterinaria

---
+++ src/components/Error.jsx
@@ -0,0 +1,19 @@
+const Error = ({ children }) => {
+    return (
+        <div className='bg-red-800 text-white text-center p-3 uppercase 
+            font-bold mb-3 rounded'>
+            <p>{children}</p>
+        </div>
+    )
+}
+
+/*const Error = ({ mensaje }) => {
+    return (
+        <div className='bg-red-800 text-white text-center p-3 uppercase 
+            font-bold mb-3 rounded'>
+            <p>{mensaje}</p>
+        </div>
+    )
+}*/
+
+export default Error
